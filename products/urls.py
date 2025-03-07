f56ea94585d9a7b56924e26cf1f7d3620a4b2cf1
Athul S <athuls062@gmail.com>
2025-03-07 14:01:34 +0530

    changed to javascript api

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('add/', views.product_add, name='product_add'),
-    path('update/<int:pk>/', views.product_update, name='product_update'),
-    path('delete/<int:pk>/', views.product_delete, name='product_delete'),
 ]
